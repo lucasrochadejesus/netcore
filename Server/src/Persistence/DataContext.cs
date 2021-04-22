8307b882f6ed651a8467ec7048c5a258b90dfb2b
projetolabsystem1@gmail.com <lucasfecap@icloud.com>
2021-04-22 17:00:26 -0300

    ef

---
+++ Server/src/Persistence/DataContext.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using Domain;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+
+        public DbSet<Evento> Eventos { get; set; }
+        public DbSet<Lote> Lotes { get; set; }
+        public DbSet<Palestrante> Palestrantes { get; set; }
+        public DbSet<PalestranteEvento> PalestrantesEventos { get; set; }
+        public DbSet<RedeSocial> RedesSociais { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PalestranteEvento>()
+                .HasKey(PE => new {PE.EventoId, PE.PalestranteId});
+        }
+    
+    }
+}
--- Server/src/WebAPI/Controllers/EventoController.cs
+++ Server/src/WebAPI/Controllers/EventoController.cs
@@ -4,8 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using WebAPI.Data;
-using WebAPI.Models;
+using Domain;
+using Persistence;
 
 namespace WebAPI.Controllers
 {
